646ba1441049a440f68961887973d72b2a21ba5b
Daniel Charão Machado <daniel.kokynhw@gmail.com>
2025-07-25 23:36:10 +0000

    oque falta para terminnar de configuraçao para usar como pwa

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,7 @@
 "use client";
 
 import { PassengerDashboard } from '@/components/passenger/PassengerDashboard';
-import { useEffect } from 'react';
 
 export default function HomePage() {
-  useEffect(() => {
-    if ('serviceWorker' in navigator) {
-      navigator.serviceWorker
-        .register('/sw.js')
-        .then((registration) => console.log('scope is: ', registration.scope))
-        .catch((e) => console.error('Service worker registration failed', e));
-    }
-  }, []);
-
   return <PassengerDashboard />;
 }
